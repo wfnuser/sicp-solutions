28d46584840dffbb2edcfe1beb31a58c11c47510
wfnuser <wfnuser@126.com>
2019-01-24 13:21:30 +0800

    segment

---
+++ ch2/e2-2.scm
@@ -0,0 +1,50 @@
+(define (x-point p)
+    (car p))
+
+(define (y-point p)
+    (cdr p))
+
+(define (make-point x y)
+    (cons x y))
+
+; ; s and e should be point
+(define (make-segment s e)
+    (cons s e))
+
+(define (start-segment seg)
+    (car seg))
+
+(define (end-segment seg)
+    (cdr seg))
+
+(define (average a b)
+    (/ (+ a b) 2))
+
+(define (average-x p-a p-b)
+    (average (x-point p-a) (x-point p-b))
+)
+
+(define (average-y p-a p-b)
+    (average (y-point p-a) (y-point p-b))
+)
+
+(define (midpoint-segment seg)
+    (let ((s (start-segment seg)))
+        (let ((e (end-segment seg)))
+            (make-point (average-x s e) (average-y s e))
+        )))
+    
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ",")
+    (display (y-point p))
+    (display ")"))
+
+(let ((p-s (make-point 4 18)))
+    (let ((p-e (make-point 5 19)))
+        (let ((seg (make-segment p-s p-e)))
+            (let (( mid (midpoint-segment seg)))
+                (print-point mid)
+            ))))
