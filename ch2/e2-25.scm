7f95b2af6531d3e23d491904c0c4d395e6f735f9
wfnuser <wfnuser@126.com>
2019-02-13 11:35:50 +0800

    2-25

---
+++ ch2/e2-25.scm
@@ -0,0 +1,7 @@
+(let ((ls1 (list 1 3 (list 5 7) 9))
+    (ls2 (list (list 7)))
+    (ls3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))))
+    (car (cdr (car (cdr (cdr ls1)))))
+    (car (car ls2))
+    (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr ls3))))))))))))
+)
