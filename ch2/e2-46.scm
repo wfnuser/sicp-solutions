dd46d643d300a8de9003286133b41bda0191d117
wfnuser <wfnuser@126.com>
2019-02-21 20:34:54 +0800

    46

--- ch2/e2-46.scm
+++ ch2/e2-46.scm
@@ -9,3 +9,23 @@
 (define (ycor-vect v)
     (cdr v)
 )
+
+(define (add-vect v1 v2)
+    (make-vector (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2)))
+)
+
+(define (sub-vect v1 v2)
+    (make-vector (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2)))
+)
+
+(define (scale-vect s v)
+    (make-vector (* (xcor-vect v) s) (* (ycor-vect v) s))
+)
+
+
+(let ((v1 (make-vector 1 2)) (v2 (make-vector 3 4)))
+    (newline)
+    (display (add-vect v1 v2))
+    (display (sub-vect v1 v2))
+    (display (scale-vect 3 v2))
+)
