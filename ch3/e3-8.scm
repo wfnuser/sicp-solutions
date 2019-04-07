158fdb67bc52c1c0a1cc5c7348773ffa229f6f03
wfnuser <wfnuser@126.com>
2019-04-07 22:34:10 +0800

    3-8

---
+++ ch3/e3-8.scm
@@ -0,0 +1,13 @@
+(define make-f
+    (let ((tmp 1) (answer 0))
+        (lambda (x) 
+            (begin (set! answer (* tmp x)) (set! tmp 0) answer)
+        )
+    )
+)
+
+(define f make-f)
+
+(f 1)
+(f 0)
+
