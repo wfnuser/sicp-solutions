5795b98c5c81fc4afbac44ef56fbe300748b11dc
wfnuser <wfnuser@126.com>
2019-04-18 15:21:52 +0800

    fix: move assoc in the compound object

--- ch3/e3-24.scm
+++ ch3/e3-24.scm
@@ -8,15 +8,22 @@
 
 (define (make-table same-key?)
     (let ((local-table (list '*table*)))
+        (define (assoc key records)
+            (cond 
+                ((null? records) false)
+                ((same-key? key (caar records)) (car records)) 
+                (else (assoc key (cdr records)))
+            )
+        )
         (define (lookup key-1 key-2) 
-            (let ((subtable (assoc key-1 (cdr local-table) same-key?))) 
+            (let ((subtable (assoc key-1 (cdr local-table)))) 
                 (if subtable
-                    (let ((record (assoc key-2 (cdr subtable) same-key?)))
+                    (let ((record (assoc key-2 (cdr subtable))))
                         (if record (cdr record) false)) false)))
         (define (insert! key-1 key-2 value) 
-            (let ((subtable (assoc key-1 (cdr local-table) same-key?))) 
+            (let ((subtable (assoc key-1 (cdr local-table)))) 
                 (if subtable
-                    (let ((record (assoc key-2 (cdr subtable) same-key?)))
+                    (let ((record (assoc key-2 (cdr subtable))))
                         (if record 
                             (set-cdr! record value) 
                             (set-cdr! subtable (cons (cons key-2 value) (cdr subtable)))))
